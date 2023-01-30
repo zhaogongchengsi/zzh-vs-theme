8c5e89c4cba7ab5eb5c08121dce8920c75183dd0
zhaogongchengsi <zzh1586169624@163.com>
2023-01-30 17:52:46 +0800

    feat: colors

--- src/colors.ts
+++ src/colors.ts
@@ -2,14 +2,50 @@
 const ZZH_THEME = {
   primary: ["#191919", "#F9F9F9"],
   foreground: ["#F4F9F9", "#333333"],
+
+  activeForeground: ["#bfbaaa", "#4e4f47"],
+  secondaryForeground: ["#dedcd590", "#393a3490"],
+  ignored: ["#dedcd530", "#393a3450"],
+  faded: ["#dedcd510", "#393a3410"],
+  border: ["#191919", "#f0f0f0"],
+
+  // token colors
+  comment: ["#758575dd", "#a0ada0"],
+  string: ["#c98a7d", "#b56959"],
+  variable: ["#bd976a", "#b07d48"],
+  keyword: ["#4d9375", "#1e754f"],
+  number: ["#4C9A91", "#2f798a"],
+  boolean: ["#4d9375", "#1e754f"],
+  operator: ["#cb7676", "#ab5959"],
+  function: ["#80a665", "#59873a"],
+  constant: ["#c99076", "#a65e2b"],
+  class: ["#6893BF", "#5a6aa6"],
+  interface: ["#5DA994", "#2e8f82"],
+  type: ["#5da9a7", "#2e808f"],
+  builtin: ["#cb7676", "#ab5959"],
+  property: ["#b8a965", "#998418"],
+  namespace: ["#db889a", "#b05a78"],
+  punctuation: ["#666666", "#999999"],
+  decorator: ["#bd8f8f", "#bd8f8f"],
+  regex: ["#c4704f", "#ab5e3f"],
+
+  // colors
+  green: ["#4d9375", "#1e754f"],
+  cyan: ["#5eaab5", "#2993a3"],
+  blue: ["#6394bf", "#296aa3"],
+  red: ["#cb7676", "#ab5959"],
+  orange: ["#d4976c", "#a65e2b"],
+  yellow: ["#e6cc77", "#bda437"],
+  magenta: ["#d9739f", "#a13865"],
 } as const;
 
-export type ColorName =  keyof typeof ZZH_THEME;
+export type ColorName = keyof typeof ZZH_THEME;
+export type Colors = Record<ColorName, string>;
 
-export function getColors(base: "vs" | "vs-dark"): Record<ColorName, string> {
+export function getColors(base: "vs" | "vs-dark"): Colors {
   const colors = {};
   for (const [name, [dark, light]] of Object.entries(ZZH_THEME)) {
     colors[name] = base === "vs" ? light : dark;
   }
-  return colors as Record<ColorName, string>;
+  return colors as Colors;
 }
--- src/tokenColor.ts
+++ src/tokenColor.ts
@@ -2,13 +2,310 @@ import { getColors, ColorName } from "./colors";
 
 export function generateTokenColors(base: "vs" | "vs-dark") {
   const basecolors = getColors(base);
-  const getColor = (key: ColorName) => basecolors[key];
+  const getColor = (key: ColorName, op:string = '') => {
+    return basecolors[key] + op
+  };
+
+  const foreground = getColor("foreground");
+  const secondaryForeground = getColor("secondaryForeground");
+  const primary = getColor("primary");
 
   return [
     {
       scope: ["comment", "punctuation.definition.comment", "string.comment"],
       settings: {
-        foreground: getColor("primary"),
+        foreground: getColor("comment"),
+      },
+    },
+    {
+      scope: [
+        "delimiter.bracket",
+        "delimiter",
+        "invalid.illegal.character-not-allowed-here.html",
+        "keyword.operator.rest",
+        "keyword.operator.spread",
+        "keyword.operator.type.annotation",
+        "keyword.operator.relational.ts",
+        "meta.brace",
+        "meta.tag.block.any.html",
+        "meta.tag.inline.any.html",
+        "meta.tag.structure.input.void.html",
+        "meta.type.annotation",
+        "storage.type.function.arrow",
+        "keyword.operator.type",
+        "meta.objectliteral.ts",
+        "punctuation",
+      ],
+      settings: {
+        foreground: getColor("punctuation"),
+      },
+    },
+    {
+      scope: [
+        "constant",
+        "entity.name.constant",
+        "variable.language",
+        "meta.definition.variable",
+      ],
+      settings: {
+        foreground: getColor("constant"),
+      },
+    },
+    {
+      scope: ["entity", "entity.name"],
+      settings: {
+        foreground: getColor("function"),
+      },
+    },
+    {
+      scope: "variable.parameter.function",
+      settings: {
+        foreground,
+      },
+    },
+    {
+      scope: ["entity.name.tag", "tag.html"],
+      settings: {
+        foreground: getColor("keyword"),
+      },
+    },
+    {
+      scope: "entity.name.function",
+      settings: {
+        foreground: getColor("function"),
+      },
+    },
+    {
+      scope: ["keyword", "storage.type.class.jsdoc"],
+      settings: {
+        foreground: getColor("keyword"),
+      },
+    },
+    {
+      scope: [
+        "storage",
+        "storage.type",
+        "support.type.builtin",
+        "constant.language.undefined",
+        "constant.language.null",
+      ],
+      settings: {
+        foreground: getColor("builtin"),
+      },
+    },
+    {
+      scope: [
+        "storage.modifier.package",
+        "storage.modifier.import",
+        "storage.type.java",
+      ],
+      settings: {
+        foreground,
+      },
+    },
+    {
+      scope: [
+        "string",
+        "string punctuation.section.embedded source",
+        "attribute.value",
+      ],
+      settings: {
+        foreground: getColor("string"),
+      },
+    },
+    {
+      scope: [
+        "punctuation.definition.string",
+        "punctuation.support.type.property-name",
+      ],
+      settings: {
+        foreground: getColor("string", "99"),
+      },
+    },
+    {
+      scope: "support",
+      settings: {
+        foreground: getColor("property"),
+      },
+    },
+    {
+      scope: [
+        "property",
+        "meta.property-name",
+        "meta.object-literal.key",
+        "entity.name.tag.yaml",
+        "attribute.name",
+      ],
+      settings: {
+        foreground: getColor("property"),
+      },
+    },
+    {
+      scope: [
+        "entity.other.attribute-name",
+        "invalid.deprecated.entity.other.attribute-name.html",
+      ],
+      settings: {
+        foreground: getColor("variable"),
+      },
+    },
+    {
+      scope: ["variable", "identifier"],
+      settings: {
+        foreground: getColor("variable"),
+      },
+    },
+    {
+      scope: ["support.type.primitive", "entity.name.type"],
+      settings: {
+        foreground: getColor("type"),
+      },
+    },
+    {
+      scope: "namespace",
+      settings: {
+        foreground: getColor("namespace"),
+      },
+    },
+    {
+      scope: ["keyword.operator", "meta.var.expr.ts"],
+      settings: {
+        foreground: getColor("operator"),
+      },
+    },
+
+    {
+      scope: "string variable",
+      settings: {
+        foreground: getColor("string"),
+      },
+    },
+    {
+      scope: ["source.regexp", "string.regexp"],
+      settings: {
+        foreground: getColor("regex"),
+      },
+    },
+    {
+      scope: [
+        "string.regexp.character-class",
+        "string.regexp constant.character.escape",
+        "string.regexp source.ruby.embedded",
+        "string.regexp string.regexp.arbitrary-repitition",
+      ],
+      settings: {
+        foreground: getColor("string"),
+      },
+    },
+    {
+      scope: "string.regexp constant.character.escape",
+      settings: {
+        foreground: getColor("yellow"),
+      },
+    },
+    {
+      scope: ["support.constant"],
+      settings: {
+        foreground: getColor("constant"),
+      },
+    },
+    {
+      scope: ["constant.numeric", "number"],
+      settings: {
+        foreground: getColor("number"),
+      },
+    },
+    {
+      scope: ["keyword.other.unit"],
+      settings: {
+        foreground: getColor("builtin"),
+      },
+    },
+    {
+      scope: ["constant.language.boolean", "constant.language"],
+      settings: {
+        foreground: getColor("boolean"),
+      },
+    },
+    {
+      scope: "meta.module-reference",
+      settings: {
+        foreground: primary,
+      },
+    },
+    {
+      scope: "punctuation.definition.list.begin.markdown",
+      settings: {
+        foreground: getColor("orange"),
+      },
+    },
+    {
+      scope: ["markup.heading", "markup.heading entity.name"],
+      settings: {
+        fontStyle: "bold",
+        foreground: primary,
+      },
+    },
+    {
+      scope: "markup.quote",
+      settings: {
+        foreground: getColor("interface"),
+      },
+    },
+    {
+      scope: "markup.italic",
+      settings: {
+        fontStyle: "italic",
+        foreground,
+      },
+    },
+    {
+      scope: "markup.bold",
+      settings: {
+        fontStyle: "bold",
+        foreground,
+      },
+    },
+    {
+      scope: "markup.raw",
+      settings: {
+        foreground: primary,
+      },
+    },
+    {
+      scope: [
+        "constant.other.reference.link",
+        "string.other.link",
+        "punctuation.definition.string.begin.markdown",
+        "punctuation.definition.string.end.markdown",
+      ],
+      settings: {
+        foreground: getColor("string"),
+      },
+    },
+    {
+      scope: ["markup.underline.link.markdown"],
+      settings: {
+        foreground: secondaryForeground,
+        fontStyle: "underline",
+      },
+    },
+    {
+      scope: ["type.identifier"],
+      settings: {
+        foreground: getColor("class"),
+      },
+    },
+    {
+      scope: ["entity.other.attribute-name.html.vue"],
+      settings: {
+        foreground: getColor("function"),
+      },
+    },
+    {
+      scope: ["invalid.illegal.unrecognized-tag.html"],
+      settings: {
+        fontStyle: "normal",
       },
     },
   ];
