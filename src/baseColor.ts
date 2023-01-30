93eb4746f04fc8b48ebc965f2431f93ecdc7e9b0
zhaogongchengsi <zzh1586169624@163.com>
2023-01-30 17:20:05 +0800

    feat: docs

--- src/baseColor.ts
+++ src/baseColor.ts
@@ -1,13 +1,20 @@
 import { getColors } from "./colors";
 const config = {
   primary: [
+    // 编辑区域的背景颜色
     "editor.background",
+    // 顶部菜单 item 背景颜色
     "menu.background",
+    // 顶部背景颜色
     "titleBar.activeBackground",
     "titleBar.inactiveBackground",
+    // 面板显示在编辑器区域下方，包含输出和集成终端等视图的背景颜色
     "panel.background",
+    // 活动栏背景颜色
     "activityBar.background",
+    // 右侧 文件树
     "sideBar.background",
+    // 编辑器区域的 tabs 颜色配置
     "editorGroupHeader.tabsBackground",
     "tab.inactiveBackground",
   ],
